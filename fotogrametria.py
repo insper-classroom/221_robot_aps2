424ab198984df4eabcf8cf1a335c7d9e7ebf4a92
Antonio <88986245+antoniohps2@users.noreply.github.com>
2022-02-22 02:35:52 -0300

    Atualizando a APS

--- fotogrametria.py
+++ fotogrametria.py
@@ -12,72 +12,116 @@ import math
 
 def encontrar_foco(D,H,h):
     """Não mude ou renomeie esta função
-        deve receber respectivamente a distancia real, o a distancia real entre os circulos e a distancia na image entre os circulos e deve retornar o foco
+    Entradas:
+       D - distancia real da câmera até o objeto (papel)
+       H - a distancia real entre os circulos (no papel)
+       h - a distancia na imagem entre os circulos
+    Saída:
+       f - a distância focal da câmera
     """
+    f = None
 
-    return None
+    return f
 
 def segmenta_circulo_ciano(hsv): 
     """Não mude ou renomeie esta função
-        deve receber uma imagem em hsv e devolver uma nova imagem com tudo em preto e os pixels do circulos ciano em branco
+    Entrada:
+        hsv - imagem em hsv
+    Saída:
+        mask - imagem em grayscale com tudo em preto e os pixels do circulos ciano em branco
     """
-
-    return None
+    mask = hsv[:,:,0]
+    
+    return mask
 
 def segmenta_circulo_magenta(hsv):
     """Não mude ou renomeie esta função
-        deve receber uma imagem em hsv e devolver uma nova imagem com tudo em preto e os pixels do circulos magenta em branco
+    Entrada:
+        hsv - imagem em hsv
+    Saída:
+        mask - imagem em grayscale com tudo em preto e os pixels do circulos magenta em branco
     """
-
-    return None
-
+    mask = hsv[:,:,0]
+    
+    return mask
 def encontrar_maior_contorno(segmentado):
     """Não mude ou renomeie esta função
-        deve receber uma imagem preta e branca e retornar APENAS o maior contorno obtido
+    Entrada:
+        segmentado - imagem em preto e branco
+    Saída:
+        contorno - maior contorno obtido (APENAS este contorno)
     """
     
-    return None
+    contorno = None    
+    return contorno
 
-def encontrar_centro_contorno(contornos):
+def encontrar_centro_contorno(contorno):
     """Não mude ou renomeie esta função
-        deve receber um contorno e retornar o centro dele, formato: (XX, YY)
+    Entrada:
+        contorno: um contorno (não o array deles)
+    Saída:
+        (Xcentro, Ycentro) - uma tuple com o centro do contorno (no formato 'int')!!! 
     """  
 
-    return (None, None)
+    Xcentro = None
+    Ycentro = None
+    
+    return (Xcentro, Ycentro)
 
 def calcular_h(centro_ciano, centro_magenta):
     """Não mude ou renomeie esta função
-        deve receber dois pontos e retornar a distancia absoluta entre eles
+    Entradas:
+        centro_ciano - ponto no formato (X,Y)
+        centro_magenta - ponto no formato (X,Y)
+    Saída:
+        distancia - a distancia Euclidiana entre os pontos de entrada 
     """
     
-    return None
-
-
-def encontrar_circulos(bgr):
-    "retorna os círculsos encontrados na imagem
-     No formato (x,y,raio)
-    "
-    return [(50,50,100), (100,100,200)]
-
+    distancia = None
+    return distancia
 
 def encontrar_distancia(f,H,h):
     """Não mude ou renomeie esta função
-        deve receber respectivamente o foco a distancia real entre os circulos e a distancia na image entre os circulos e retornar a distancia real
+    Entrada:
+        f - a distância focal da câmera
+        H - A distância real entre os pontos no papel
+        h - a distância entre os pontos na imagem
+    Saída:
+        D - a distância do papel até câmera
     """
-
-    return None
+    D = None
+    return D
 
 def calcular_distancia_entre_circulos(img):
     """Não mude ou renomeie esta função
-        deve utilizar as funções acima para retornar a disntacia entre os circulos, o centro_ciano, o centro_magenta e a imagem com os contornos desenhados
+    Deve utilizar as funções acima para calcular a distancia entre os circulos a partir da imagem BGR
+    Entradas:
+        img - uma imagem no formato BGR
+    Saídas:
+        h - a distância entre os os circulos na imagem
+        centro ciano - o centro do círculo ciano no formato (X,Y)
+        centro_magenta - o centro do círculo magenta no formato (X,Y)
+        img_contornos - a imagem com os contornos desenhados
     """
+    img_contornos = img.copy()
+    
+    centro_magenta = None
+    centro_ciano = None
+    
+    h = None
 
-    return None, None, None, None
+    return h, centro_ciano, centro_magenta, img_contornos
 
-def calcular_angulo_com_horinzontal_da_imagem(centro_ciano, centro_magenta):
+def calcular_angulo_com_horizontal_da_imagem(centro_ciano, centro_magenta):
     """Não mude ou renomeie esta função
-        deve calcular o angulo, em radiano, entre o vetor formato com os centros do circulos e a horizontal.
-        Retornar o angulo em graos
+        Deve calcular o angulo, em graus, entre o vetor formato com os centros do circulos e a horizontal.
+    Entradas:
+        centro_ciano - centro do círculo ciano no formato (X,Y)
+        centro_magenta - centro do círculo magenta no formato (X,Y)
+    Saídas:
+        angulo - o ângulo entre os pontos em graus
     """
-
-    return None
+    
+    angulo = None
+ 
+    return angulo
--- webcam.py
+++ webcam.py
@@ -10,7 +10,17 @@ import fotogrametria
 
 # ->>> !!!! FECHE A JANELA COM A TECLA ESC !!!! <<<<-
 
-def calcular_e_desenhar_na_image_da_webcam(img):
+def calcular_angulo_e_distancia_na_image_da_webcam(img, f):
+    """Não mude ou renomeie esta função
+        ->>> !!!! FECHE A JANELA COM A TECLA ESC !!!! <<<<-
+        deve receber a imagem da camera e retornar uma imagems com os contornos desenhados e os valores da distancia e o angulo.
+    """
+    D = 0
+    angulo = 0
+
+    return img, D, angulo
+
+def desenhar_na_image_da_webcam(img, distancia, angulo):
     """Não mude ou renomeie esta função
         ->>> !!!! FECHE A JANELA COM A TECLA ESC !!!! <<<<-
         deve receber a imagem da camera e retornar uma imagems com os contornos desenhados e a distancia e o angulo escrito em um canto da imagem.
@@ -18,22 +28,26 @@ def calcular_e_desenhar_na_image_da_webcam(img):
 
     return img
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
 
+    ## -> Mude o Foco <- ##
+    f = fotogrametria.encontrar_foco(1, 1, 1)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
 
-while rval:
-    img = calcular_e_desenhar_na_image_da_webcam(frame)
-    cv2.imshow("preview", img)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+    while rval:
+        img, distancia, angulo = calcular_angulo_e_distancia_na_image_da_webcam(frame, f)
+        img = desenhar_na_image_da_webcam(img, distancia, angulo)
+        cv2.imshow("preview", img)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
 
-cv2.destroyWindow("preview")
-vc.release()
+    cv2.destroyWindow("preview")
+    vc.release()
